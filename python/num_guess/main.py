35e6dc2ba749f3bc979ce5e595cfc94f13457c4b
sky <jordanlam16@outlook.com>
2024-05-29 12:17:22 +0800

    unify texts

--- python/num_guess/main.py
+++ python/num_guess/main.py
@@ -7,18 +7,18 @@ def guess_num():
 
     while True:
         if max_tries == 0:
-            print("You have reach the max tries of 3 times!")
+            print("Sorry you have reach max tries of 3 times!")
             return 
 
-        guess = input("Please enter a number. (below 10): ")
+        guess = input("Please enter your guess.(below 10): ")
 
         if not guess.isdigit():  
-            print("Please enter a valid integer.")  
+            print("Please type a valid integer!")  
             max_tries -=1
         else:  
             guessInt = int(guess)
             if guessInt > 11:
-                print("Please enter a number below 10.")
+                print("Please enter a number below 10!")
                 max_tries -=1
             else:
                 if compare_vals(guessInt,answer): return 
@@ -27,11 +27,11 @@ def compare_vals(guess,answer):
 
 
     # print(f"The correct number is {correct}")
-    print(f"Your guessed: {guess}")
+    print(f"You guessed: {guess}")
     # print(f"Your guessed: {guess}, The correct is: {answer}")
 
     if guess == answer:
-        print("Congrats, you guess correctly!")
+        print("Congrats, Your answer is Correct!")
         return True
     else:
         # print("Sorry, you guessed wrong.")
