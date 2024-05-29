8e98a4de703ffae726ec23f0a8ce82f1b637131e
sky <jordanlam16@outlook.com>
2024-05-29 12:35:37 +0800

    add ignore

--- rust/data_control/src/main.rs
+++ rust/data_control/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+
+use polars::prelude::*;  
+
+fn read_csv(file){
+    // Read a CSV file into a DataFrame
+
+    if let Ok(df) = CsvReader::from_path(file)  
+        .unwrap()  
+        .finish()  
+    {  
+        return df;
+    } else {  
+        println!("Failed to read the CSV file");  
+    }  
 }
+
+fn get_data(){
+    path = "example.csv";
+    data = read_csv(path);
+
+    query = data[data['a'] == 0 ];
+    println!(query);
+}
+
+
+pub fn main() {  
+  
+
+}  
